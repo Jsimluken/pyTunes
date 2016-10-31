517436228bed5504832730ba372fe69ff22d3e5b
jsimluken <simonsluke134@gmail.com>
2016-10-31 15:24:37 +0900

    とりあえずサンプルコード

---
+++ audio.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+import wave
+import pyaudio
+
+CHUNK = 1024
+
+wf = wave.open(filename,"rb")
+p = pyaudio.PyAudio()
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+data = wf.readframes(CHUNK) 
+
+while data != '':
+    stream.write(data)
+    data = readframes(CHUNK)
+    
+stream.stop_stream()
+stream.close()
+
+p.terminate()
