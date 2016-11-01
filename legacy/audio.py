66f1f6f07a96c5448b53bc49f473f9ae653173c4
jsimluken <simonsluke134@gmail.com>
2016-11-01 19:05:08 +0900

    余計なものを削除

--- legacy/audio.py
+++ legacy/audio.py
@@ -5,10 +5,12 @@ import pyaudio
 CHUNK = 1024
 filename = "../Assets/Roundabout.wav"
 wf = wave.open(filename,"rb")
+print wf
 p = pyaudio.PyAudio()
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 channels=wf.getnchannels(),
                 rate=wf.getframerate(),
+                #rate = 40000,
                 output=True)
 data = wf.readframes(CHUNK) 
 
